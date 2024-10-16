10f01f4ae95ef736052f2f65807333fddbfa5849
raisakuna <sakunaxl@gmail.com>
2024-10-16 19:00:48 -0400

    gateway deleted

--- SEBank/src/main/java/com/seleniumexpress/sebank/restcontrollers/AccountController.java
+++ SEBank/src/main/java/com/seleniumexpress/sebank/restcontrollers/AccountController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seleniumexpress.sebank.restcontrollers.models.CreateAccountRequestModel;
-import com.seleniumexpress.sebank.restcontrollers.models.CreateAccountRequestModeloriginal;
+import com.seleniumexpress.sebank.restcontrollers.models.CreateAccountRequestModel;
 
 @RestController
 @RequestMapping("/account")
--- SEBank/src/main/java/com/seleniumexpress/sebank/restcontrollers/models/CreateAccountRequestModel.java
+++ SEBank/src/main/java/com/seleniumexpress/sebank/restcontrollers/models/CreateAccountRequestModel.java
@@ -5,26 +5,26 @@ import java.util.UUID;
 public class CreateAccountRequestModel {
 	
 	private UUID accountNumber;
-private String accountHolderName;
-private int createdYear;
-public UUID getAccountNumber() {
-	return accountNumber;
-}
-public void setAccountNumber(UUID accountNumber) {
-	this.accountNumber = accountNumber;
-}
-public String getAccountHolderName() {
-	return accountHolderName;
-}
-public void setAccountHolderName(String accountHolderName) {
-	this.accountHolderName = accountHolderName;
-}
-public int getCreatedYear() {
-	return createdYear;
-}
-public void setCreatedYear(int createdYear) {
-	this.createdYear = createdYear;
-}
-
+	private String accountHolderName;
+	private int createdYear;
+	
+	public UUID getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(UUID accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+	public int getCreatedYear() {
+		return createdYear;
+	}
+	public void setCreatedYear(int createdYear) {
+		this.createdYear = createdYear;
+	}
 
 }
